0bfb747bfea7cb5fd8a44ed829369da60021e972
grant-humphries <grant.humphries@gmail.com>
2016-07-27 14:21:43 -0700

    added cram as a testing dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ogr2osm',
     version='0.1.0',
+
     author='Paul Norman, Andrew Guertin, Ivan Ortega',
     description='A tool for converting ogr-readable files like shapefiles '
                 'into .osm data',
@@ -11,6 +12,7 @@ setup(
             'ogr2osm = ogr2osm.ogr2osm:main'
         ]
     },
+    extras_require=dict(test=['cram>=0.7']),
     install_requires=['gdal>=1.10.0'],
     license='Beerware/MIT',
     long_description=open('Readme.md').read(),
